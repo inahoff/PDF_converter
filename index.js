593517ac98975ec8672c1792f71890d61fdf0364
maiachb <77111034+maiachb@users.noreply.github.com>
2022-06-16 12:17:00 +0200

    Endringer
    +++

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+<script src="js/jsPDF/dist/jspdf.min.js"></script>
 
+function Convert() {
+
+var doc = new jsPDF();
+
+doc.text(20, 20, 'Hello world!');
+doc.text(20, 30, 'This is client-side Javascript to generate a PDF.');
+
+
+
+doc.save('document.pdf');
+}
